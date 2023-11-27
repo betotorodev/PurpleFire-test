65a0404b2e4ed5301a4bf340972613bd688ee8a4
Beto Toro <betotoro0902@gmail.com>
2023-11-27 16:15:22 -0500

    fix: add scroll behaviour

--- app/page.js
+++ app/page.js
@@ -17,58 +17,64 @@ export default function Home() {
   return (
     <main className='w-full min-h-screen'>
       {/* hero */}
-      <section className='flex w-full'>
-        <div className='px-[150px] py-[55px] bg-[#F3EEE8] w-1/2 h-screen'>
-          <nav className='flex items-center h-fit'>
-            <h1 className={`${merriweather.variable} font-serif text-[34px] font-bold mr-8 tracking-[2px] leading-[25px]`}>antic</h1>
-            <ul className={`flex gap-6 ${varta.variable} font-sans text-[#706458]`}>
-              <li>Products</li>
-              <li>Rooms</li>
-              <li>Services</li>
-              <li>Inspirations</li>
-            </ul>
-          </nav>
-          <div className='mt-[135px]'>
-            <h2 className={`${varta.variable} font-sans text-sm font-bold mb-[50px] leading-[1px]`}>HOME DESIGN</h2>
-            <h1 className={`${merriweather.variable} font-serif text-[50px] font-light leading-[70px] mb-[30px]`}>Elegance for Interiors & Exteriors</h1>
-            <p className={`${varta.variable} font-sans text-[22px] font-light leading-[30.8px] mb-12 text-[#706458E5]`}>We provide everyone with modern, trendy, quality furniture</p>
-            <ArrowBrownDown />
+      <section className='w-full bg-[#F3EEE8]'>
+        <div className='flex max-w-[1440px] my-0 mx-auto'>
+          <div className='px-[150px] py-[55px] w-1/2 h-screen'>
+            <nav className='flex items-center h-fit'>
+              <h1 className={`${merriweather.variable} font-serif text-[34px] font-bold mr-8 tracking-[2px] leading-[25px]`}>antic</h1>
+              <ul className={`flex gap-6 ${varta.variable} font-sans text-[#706458]`}>
+                <li>Products</li>
+                <li>Rooms</li>
+                <li>Services</li>
+                <li>Inspirations</li>
+              </ul>
+            </nav>
+            <div className='mt-[135px]'>
+              <h2 className={`${varta.variable} font-sans text-sm font-bold mb-[50px] leading-[1px]`}>HOME DESIGN</h2>
+              <h1 className={`${merriweather.variable} font-serif text-[50px] font-light leading-[70px] mb-[30px]`}>Elegance for Interiors & Exteriors</h1>
+              <p className={`${varta.variable} font-sans text-[22px] font-light leading-[30.8px] mb-12 text-[#706458E5]`}>We provide everyone with modern, trendy, quality furniture</p>
+              <a href='#hero-section-two'>
+                <ArrowBrownDown />
+              </a>
+            </div>
           </div>
-        </div>
-        <div className='w-full h-screen overflow-hidden relative'>
-          <div className='absolute right-[40px] top-[40px]'>
-            <Contact />
+          <div className='w-full h-screen overflow-hidden relative'>
+            <div className='absolute right-[40px] top-[40px]'>
+              <Contact />
+            </div>
+            <Image src={HeroImage} alt='Hero' priority />
           </div>
-          <Image src={HeroImage} alt='Hero' priority />
         </div>
       </section>
       {/* hero part two */}
-      <section className='flex gap-12 w-full px-[150px] py-[70px] bg-[#F3EEE8]'>
-        <div>
-          <h1 className={`${merriweather.variable} w-[458px] font-serif text-[55px] text-[#706458] font-light mr-8 mb-[30px] tracking-[-0.6px] leading-[60px]`}>We stay by your side to design your projects</h1>
-          <a className={`${karla.variable} font-action flex items-center text-[17px] font-bold leading-[25px] tracking-[0.3px] text-[#A06056] cursor-pointer`}>Find out more <ArrowLightRight /></a>
+      <section className='w-full bg-[#F3EEE8]' id='hero-section-two'>
+        <div className='max-w-[1440px] my-0 mx-auto flex gap-12 px-[150px] py-[70px]'>
+          <div>
+            <h1 className={`${merriweather.variable} w-[458px] font-serif text-[55px] text-[#706458] font-light mr-8 mb-[30px] tracking-[-0.6px] leading-[60px]`}>We stay by your side to design your projects</h1>
+            <a className={`${karla.variable} font-action flex items-center text-[17px] font-bold leading-[25px] tracking-[0.3px] text-[#A06056] cursor-pointer`}>Find out more <ArrowLightRight /></a>
+          </div>
+          <article className='flex flex-wrap gap-y-[30px] pt-5'>
+            <div className='w-[259px]'>
+              <h3 className={`${varta.variable} font-sans font-semibold text-[#A06056] text-[25px] mb-3`}>Our Advices</h3>
+              <p className={`${varta.variable} font-sans font-normal text-[#706458E5] text-lg leading-[25px]`}>What if you took advantage of this rather special period to rethink your interior? You spend more time at home so you might as well feel good there.</p>
+            </div>
+            <div className='w-[259px]'>
+              <h3 className={`${varta.variable} font-sans font-semibold text-[#A06056] text-[25px] mb-3`}>Click and Collect</h3>
+              <p className={`${varta.variable} font-sans font-normal text-[#706458E5] text-lg leading-[25px]`}>Adapted and contactless. Free from € 200 of purchases in stores offering the service.</p>
+            </div>
+            <div className='w-[259px]'>
+              <h3 className={`${varta.variable} font-sans font-semibold text-[#A06056] text-[25px] mb-3`}>Conception Service</h3>
+              <p className={`${varta.variable} font-sans font-normal text-[#706458E5] text-lg leading-[25px]`}>Personalized service with an expert advisor for your interior and exterior decoration or layout projects.</p>
+            </div>
+            <div className='w-[259px]'>
+              <h3 className={`${varta.variable} font-sans font-semibold text-[#A06056] text-[25px] mb-3`}>Installation Service</h3>
+              <p className={`${varta.variable} font-sans font-normal text-[#706458E5] text-lg leading-[25px]`}>Our furniture packaged in flat packs are designed for easy assembly. But you can call on our partner.</p>
+            </div>
+          </article>
         </div>
-        <article className='flex flex-wrap gap-y-[30px] pt-5'>
-          <div className='w-[259px]'>
-            <h3 className={`${varta.variable} font-sans font-semibold text-[#A06056] text-[25px] mb-3`}>Our Advices</h3>
-            <p className={`${varta.variable} font-sans font-normal text-[#706458E5] text-lg leading-[25px]`}>What if you took advantage of this rather special period to rethink your interior? You spend more time at home so you might as well feel good there.</p>
-          </div>
-          <div className='w-[259px]'>
-            <h3 className={`${varta.variable} font-sans font-semibold text-[#A06056] text-[25px] mb-3`}>Click and Collect</h3>
-            <p className={`${varta.variable} font-sans font-normal text-[#706458E5] text-lg leading-[25px]`}>Adapted and contactless. Free from € 200 of purchases in stores offering the service.</p>
-          </div>
-          <div className='w-[259px]'>
-            <h3 className={`${varta.variable} font-sans font-semibold text-[#A06056] text-[25px] mb-3`}>Conception Service</h3>
-            <p className={`${varta.variable} font-sans font-normal text-[#706458E5] text-lg leading-[25px]`}>Personalized service with an expert advisor for your interior and exterior decoration or layout projects.</p>
-          </div>
-          <div className='w-[259px]'>
-            <h3 className={`${varta.variable} font-sans font-semibold text-[#A06056] text-[25px] mb-3`}>Installation Service</h3>
-            <p className={`${varta.variable} font-sans font-normal text-[#706458E5] text-lg leading-[25px]`}>Our furniture packaged in flat packs are designed for easy assembly. But you can call on our partner.</p>
-          </div>
-        </article>
       </section>
       {/* call to action */}
-      <section className='flex justify-between items-center px-[150px] py-[70px]'>
+      <section className='max-w-[1440px] my-0 mx-auto flex justify-between items-center px-[150px] py-[70px]'>
         <article>
           <h2 className={`${merriweather.variable} font-serif text-[35px] text-[#121212] font-light mb-[15px] tracking-[-0.3px] leading-[40px]`}>Be aware of the latest trends</h2>
           <p className={`${varta.variable} font-sans font-normal text-[#706458E5] text-lg leading-[25px] mb-[10px]`}>Stay informed of new trends, but also of our various offers.</p>
@@ -82,7 +88,7 @@ export default function Home() {
       {/* carrousel */}
       <section className='pt-16 pb-[70px]'>
         <Carousel />
-        <article className='flex justify-between px-[150px] mt-[40px]'>
+        <article className='max-w-[1440px] my-0 mx-auto flex justify-between px-[150px] mt-[40px]'>
           <div className='flex gap-[30px]'>
             <h2 className={`${merriweather.variable} font-serif text-[35px] w-[360px] text-[#121212] font-light mb-[15px] tracking-[-0.3px] leading-[40px]`}>Inspirations</h2>
             <p className={`${varta.variable} font-sans font-normal w-[359px] text-[#706458E5] text-lg leading-[25px]`}>Our experts are keen to stay on top of trends in order to offer you new inspirations for your interior and exterior every day. Remember that to inspire you we have to inspire ourselves and we want to share that with you.</p>
@@ -97,69 +103,71 @@ export default function Home() {
         </article>
       </section>
       {/* footer */}
-      <footer className={`${varta.variable} font-sans py-[70px] px-[150px] bg-[#F3EEE8] text-[#706458]`}>
-        <article className='flex justify-between gap-x-16 mb-12'>
-          <h1 className={`${merriweather.variable} text-[#534B42] font-serif text-[34px] font-bold mr-8 tracking-[2px] leading-[25px]`}>antic</h1>
-          <div>
-            <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>Products</h3>
-            <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
-              <li>Furniture</li>
-              <li>Decoration</li>
-              <li>Storage</li>
-              <li>Baby and child</li>
-              <li>Connected home</li>
+      <footer className={`${varta.variable} font-sans bg-[#F3EEE8] text-[#706458]`}>
+        <div className='max-w-[1440px] my-0 mx-auto py-[70px] px-[150px]'>
+          <article className='flex justify-between gap-x-16 mb-12'>
+            <h1 className={`${merriweather.variable} text-[#534B42] font-serif text-[34px] font-bold mr-8 tracking-[2px] leading-[25px]`}>antic</h1>
+            <div>
+              <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>Products</h3>
+              <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+                <li>Furniture</li>
+                <li>Decoration</li>
+                <li>Storage</li>
+                <li>Baby and child</li>
+                <li>Connected home</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>Rooms</h3>
+              <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+                <li>Living room</li>
+                <li>Dining room</li>
+                <li>Cooked</li>
+                <li>Bedroom</li>
+                <li>Office</li>
+                <li>Laundry</li>
+                <li>Garage</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>Services</h3>
+              <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+                <li>Click and collect</li>
+                <li>Conception</li>
+                <li>Installation</li>
+                <li>Advices</li>
+                <li>Gift card</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>About</h3>
+              <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+                <li>Our story</li>
+                <li>Our stores</li>
+                <li>Our partners</li>
+              </ul>
+            </div>
+            <div className='max-w-[243px]'>
+              <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>We respect our planet</h3>
+              <Image className='mb-[15px]' src={FooterImage} width={243} height={180} alt='footer image' />
+              <p className='mb-[15px] opacity-70'>We’re taking positive steps to reduce our impact on the planet. For us, that means retailing responsibly.</p>
+              <a className={`${karla.variable} font-action flex items-center text-[17px] font-bold leading-[25px] tracking-[0.3px] text-[#A06056] cursor-pointer`}>Learn more <ArrowLightRight /></a>
+            </div>
+          </article>
+          <div className='flex justify-center w-full gap-6 mb-5'>
+            <Facebook />
+            <Instagram />
+            <Linkedin />
+            <Twitter />
+          </div>
+          <div className='flex justify-between items-center opacity-70'>
+            <span>© 2021 Agence Dnd</span>
+            <ul className='flex gap-[30px]'>
+              <li>Privacy policy</li>
+              <li>Term of service</li>
+              <li>Language</li>
             </ul>
           </div>
-          <div>
-            <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>Rooms</h3>
-            <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
-              <li>Living room</li>
-              <li>Dining room</li>
-              <li>Cooked</li>
-              <li>Bedroom</li>
-              <li>Office</li>
-              <li>Laundry</li>
-              <li>Garage</li>
-            </ul>
-          </div>
-          <div>
-            <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>Services</h3>
-            <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
-              <li>Click and collect</li>
-              <li>Conception</li>
-              <li>Installation</li>
-              <li>Advices</li>
-              <li>Gift card</li>
-            </ul>
-          </div>
-          <div>
-            <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>About</h3>
-            <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
-              <li>Our story</li>
-              <li>Our stores</li>
-              <li>Our partners</li>
-            </ul>
-          </div>
-          <div className='max-w-[243px]'>
-            <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>We respect our planet</h3>
-            <Image className='mb-[15px]' src={FooterImage} width={243} height={180} alt='footer image' />
-            <p className='mb-[15px] opacity-70'>We’re taking positive steps to reduce our impact on the planet. For us, that means retailing responsibly.</p>
-            <a className={`${karla.variable} font-action flex items-center text-[17px] font-bold leading-[25px] tracking-[0.3px] text-[#A06056] cursor-pointer`}>Learn more <ArrowLightRight /></a>
-          </div>
-        </article>
-        <div className='flex justify-center w-full gap-6 mb-5'>
-          <Facebook />
-          <Instagram />
-          <Linkedin />
-          <Twitter />
-        </div>
-        <div className='flex justify-between items-center opacity-70'>
-          <span>© 2021 Agence Dnd</span>
-          <ul className='flex gap-[30px]'>
-            <li>Privacy policy</li>
-            <li>Term of service</li>
-            <li>Language</li>
-          </ul>
         </div>
       </footer>
     </main>
