eafb7037cc27742c086e8c5de7e0f2ee0567240e
Beto Toro <betotoro0902@gmail.com>
2023-11-27 15:51:19 -0500

    style: finish footer

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
 import Image from 'next/image'
 import HeroImage from '@/public/hero-image.webp'
+import FooterImage from '@/public/image-footer.webp'
 import { varta, merriweather, karla } from '@/app/fonts'
 import { ArrowBrownDown } from '@/app/ui/icons/arrow-brown-down'
 import { ArrowLightRight } from '@/app/ui/icons/arrow-light-right'
 import { Carousel } from '@/app/ui/carousel'
-import { ArrowBrownRight } from './ui/icons/arrow-brown-right'
-import { ArrowBrownLeft } from './ui/icons/arrow-brown-left'
+import { ArrowBrownRight } from '@/app/ui/icons/arrow-brown-right'
+import { ArrowBrownLeft } from '@/app/ui/icons/arrow-brown-left'
+import { Facebook } from '@/app/ui/icons/facebook'
+import { Instagram, instgram } from '@/app/ui/icons/instagram'
+import { Linkedin } from '@/app/ui/icons/linkedin'
+import { Twitter } from '@/app/ui/icons/twitter'
 
 export default function Home() {
   return (
@@ -87,6 +92,72 @@ export default function Home() {
           </div>
         </article>
       </section>
+      {/* footer */}
+      <footer className={`${varta.variable} font-sans py-[70px] px-[150px] bg-[#F3EEE8] text-[#706458]`}>
+        <article className='flex justify-between gap-x-16 mb-12'>
+          <h1 className={`${merriweather.variable} text-[#534B42] font-serif text-[34px] font-bold mr-8 tracking-[2px] leading-[25px]`}>antic</h1>
+          <div>
+            <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>Products</h3>
+            <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+              <li>Furniture</li>
+              <li>Decoration</li>
+              <li>Storage</li>
+              <li>Baby and child</li>
+              <li>Connected home</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>Rooms</h3>
+            <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+              <li>Living room</li>
+              <li>Dining room</li>
+              <li>Cooked</li>
+              <li>Bedroom</li>
+              <li>Office</li>
+              <li>Laundry</li>
+              <li>Garage</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>Services</h3>
+            <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+              <li>Click and collect</li>
+              <li>Conception</li>
+              <li>Installation</li>
+              <li>Advices</li>
+              <li>Gift card</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>About</h3>
+            <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+              <li>Our story</li>
+              <li>Our stores</li>
+              <li>Our partners</li>
+            </ul>
+          </div>
+          <div className='max-w-[243px]'>
+            <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>We respect our planet</h3>
+            <Image className='mb-[15px]' src={FooterImage} width={243} height={180} alt='footer image' />
+            <p className='mb-[15px] opacity-70'>We’re taking positive steps to reduce our impact on the planet. For us, that means retailing responsibly.</p>
+            <a className={`${karla.variable} font-action flex items-center text-[17px] font-bold leading-[25px] tracking-[0.3px] text-[#A06056] cursor-pointer`}>Learn more <ArrowLightRight /></a>
+          </div>
+        </article>
+        <div className='flex justify-center w-full gap-6 mb-5'>
+          <Facebook />
+          <Instagram />
+          <Linkedin />
+          <Twitter />
+        </div>
+        <div className='flex justify-between items-center opacity-70'>
+          <span>© 2021 Agence Dnd</span>
+          <ul className='flex gap-[30px]'>
+            <li>Privacy policy</li>
+            <li>Term of service</li>
+            <li>Language</li>
+          </ul>
+        </div>
+      </footer>
     </main>
   )
 }
