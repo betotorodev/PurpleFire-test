89923d6b934f10c324ad6935c0cb2129bdc84295
Beto Toro <betotoro0902@gmail.com>
2023-11-28 06:44:14 -0500

    style: finish contact

--- app/product-detail/page.js
+++ app/product-detail/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import Logo from '@/public/Needus.webp'
+import Contact from '@/public/contact.webp'
 import { ArrowLightDown } from '@/app/ui/icons/arrow-light-down'
 import { Search } from '../ui/icons/search'
 import { Like } from '../ui/icons/like'
 import { Cart } from '../ui/icons/cart'
+import { Menu } from '../ui/icons/menu'
 
 export default function ProductDetail() {
   return (
@@ -41,6 +43,25 @@ export default function ProductDetail() {
           </div>
         </div>
       </section>
+      <header className='flex justify-between bg-[#2F333A] w-full h-[76px]'>
+        <span className='flex items-center bg-[#E73C17] pl-[58px] pr-[48px] text-white gap-[17px] text-2xl font-medium'><Menu /> All Categories <ArrowLightDown width='30' height='30' /></span>
+        <nav className='flex border-r-2 border-white'>
+          <ul className='flex gap-[21px] text-white pl-[129px] py-[28px] pr-[88px]'>
+            <li className='flex items-center gap-2'>HOME <ArrowLightDown /></li>
+            <li className='flex items-center gap-2'>ABOUT <ArrowLightDown /></li>
+            <li className='flex items-center gap-2 text-[#E73C17]'>PRODUCT <ArrowLightDown /></li>
+            <li className='flex items-center gap-2'>PAGES <ArrowLightDown /></li>
+            <li className='flex items-center gap-2'>CONTACT <ArrowLightDown /></li>
+          </ul>
+        </nav>
+        <div className='flex items-center gap-[25px] pr-20 pl-5 text-white'>
+          <Image src={Contact} alt='Contact' />
+          <div className='flex flex-col'>
+            <span>Contact Us 24/7</span>
+            <span>+ 12012987481</span>
+          </div>
+        </div>
+      </header>
     </div>
   )
 }
--- app/ui/icons/arrow-light-down.js
+++ app/ui/icons/arrow-light-down.js
@@ -1,11 +1,11 @@
-export const ArrowLightDown = ({ color = 'white' }) => (
-  <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 20 20' fill='none'>
+export const ArrowLightDown = ({ color = 'white', width = '20', height = '20' }) => (
+  <svg xmlns='http://www.w3.org/2000/svg' width={width} height={height} viewBox='0 0 20 20' fill='none'>
     <g clip-path='url(#clip0_36_750)'>
       <path d='M5 7.5L10 12.5L15 7.5' stroke={color} strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round' />
     </g>
     <defs>
       <clipPath id='clip0_36_750'>
-        <rect width='20' height='20' fill={color} />
+        <rect width={width} height={height} fill={color} />
       </clipPath>
     </defs>
   </svg>
