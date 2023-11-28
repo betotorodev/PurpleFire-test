591f6156bb3ac06f0822fc0791e3a9f452e93597
Beto Toro <betotoro0902@gmail.com>
2023-11-28 12:43:10 -0500

    style: finish footer layout

--- app/product-detail/page.js
+++ app/product-detail/page.js
@@ -18,6 +18,12 @@ import { Menu } from '../ui/icons/menu'
 import { StarEmpty } from '../ui/icons/star-empty'
 import { StarFill } from '../ui/icons/star-fill'
 import { LikeFill } from '../ui/icons/like-fill'
+import { Whatsapp } from '../ui/icons/whatsapp'
+import { FacebookTwo } from '../ui/icons/facebook-2'
+import { InstagramTwo } from '../ui/icons/instagram-2'
+import { Youtube } from '../ui/icons/youtube'
+import { Cellphone } from '../ui/icons/cellphone'
+import { Location } from '../ui/icons/location'
 
 const relatedProducts = [
   {
@@ -251,6 +257,69 @@ export default function ProductDetail() {
           <button className='h-full text-[20px] text-white w-[268px] bg-[#E73C17]'>SUBSCRIBE</button>
         </form>
       </section>
+      {/* footer */}
+      <footer className='bg-[#2F333A] text-[white]'>
+        <div className='max-w-[1440px] my-0 mx-auto py-[58px] pl-20 pr-10'>
+          <article className='flex justify-between gap-x-[47px] mb-12'>
+            <div className='pr-[27px]'>
+              <h1 className='text-white text-[24px] font-medium mb-[29px]'>ABOUT US</h1>
+              <p className='text-lg leading-[30px] mb-[29px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lacinia nulla ut laoreet. Quisque ultricies et tortor nec laoreet. </p>
+              <ul className='flex gap-[10px]'>
+                <li><FacebookTwo /></li>
+                <li><Whatsapp /></li>
+                <li><InstagramTwo /></li>
+                <li><Youtube /></li>
+              </ul>
+            </div>
+            <div>
+              <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>INFORMATION</h3>
+              <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+                <li>About</li>
+                <li>Delivery information</li>
+                <li>Privacy Policy</li>
+                <li>Sales</li>
+                <li>Terms & Conditions</li>
+                <li>EMI Payment</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>ACCOUNT</h3>
+              <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+                <li>My Account</li>
+                <li>My Orders</li>
+                <li>Returns</li>
+                <li>Shipping</li>
+                <li>Wishlist</li>
+                <li>Account Details</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>STORE</h3>
+              <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+                <li>Affiliate</li>
+                <li>Discounts</li>
+                <li>Sale</li>
+                <li>Contact</li>
+                <li>All Collections</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className='text-[25px] font-semibold leading-[30px] tracking-[-0.2px] mb-[23px]'>CONTACT US</h3>
+              <p className='mb-[21px]'>If you have any query, please contact us <a className='text-[#E73C17] font-medium' href='#'>needus24@gmail.com</a></p>
+              <ul className='flex flex-col gap-3 text-lg font-normal opacity-70'>
+                <li className='flex items-center gap-1 text-white'><Location /> California, USA</li>
+                <li className='flex items-center gap-1 text-white'><Cellphone /> + 12012987481</li>
+              </ul>
+            </div>
+          </article>
+        </div>
+        <div className='pl-20'>
+          <div className='w-full border opacity-50 border-[#F0F0F0]' />
+        </div>
+        <div className='flex justify-center items-center text-[15px] py-[90px]'>
+          <span>Copyright. 2023 All Right Reserved</span>
+        </div>
+      </footer>
     </div>
   )
 }
