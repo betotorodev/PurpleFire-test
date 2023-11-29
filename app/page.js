2df8eea6f2eee1a62faa1f3cd4d307c6fe2b6705
Beto Toro <betotoro0902@gmail.com>
2023-11-29 16:14:20 -0500

    feat: componentize hero section

--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 import Image from 'next/image'
-import HeroImage from '@/public/hero-image.webp'
 import FooterImage from '@/public/image-footer.webp'
 import { varta, merriweather, karla } from '@/app/fonts'
-import { ArrowBrownDown } from '@/app/ui/icons/arrow-brown-down'
 import { ArrowLightRight } from '@/app/ui/icons/arrow-light-right'
 import { Carousel } from '@/app/ui/carousel'
 import { ArrowBrownRight } from '@/app/ui/icons/arrow-brown-right'
@@ -11,48 +9,12 @@ import { Facebook } from '@/app/ui/icons/facebook'
 import { Instagram } from '@/app/ui/icons/instagram'
 import { Linkedin } from '@/app/ui/icons/linkedin'
 import { Twitter } from './ui/icons/twitter-icon'
-import { MenuMobile } from './ui/icons/menu-mobile'
-import { Contact } from './ui/icons/contact'
+import { Hero } from '@/app/ui/landing/hero'
 
 export default function Home() {
   return (
     <main className='w-full min-h-screen'>
-      {/* hero */}
-      <section className='w-full bg-[#F3EEE8]'>
-        <div className='relative flex flex-col md:flex-row max-w-[1440px] my-0 mx-auto'>
-          <div className='absolute z-10 md:relative order-2 md:order-1 px-[31px] md:px-[150px] py-[51px] md:py-[55px] md:w-1/2 h-screen'>
-            <nav className='flex items-center justify-between md:justify-normal h-fit'>
-              <h1 className={`${merriweather.variable} font-serif text-[34px] text-white md:text-[#A06056] font-bold mr-8 tracking-[2px] leading-[25px]`}>antic</h1>
-              <div className='block md:hidden'>
-                <MenuMobile />
-              </div>
-              <ul className={`hidden md:flex gap-6 ${varta.variable} font-sans text-[#706458]`}>
-                <li>Products</li>
-                <li>Rooms</li>
-                <li>Services</li>
-                <li>Inspirations</li>
-              </ul>
-            </nav>
-            <div className='mt-[91px] md:mt-[135px] text-center md:text-left'>
-              <h2 className={`${varta.variable} font-sans text-sm text-white md:text-[#A06056] font-bold mb-6 md:mb-[50px] leading-[1px]`}>HOME DESIGN</h2>
-              <h1 className={`${merriweather.variable} font-serif text-[35px] md:text-[50px] text-white md:text-[#A06056] font-light leading-[40px] md:leading-[70px] mb-6 md:mb-[30px]`}>Elegance for Interiors & Exteriors</h1>
-              <p className={`${varta.variable} font-sans text-lg md:text-[22px] font-light leading-[25px] md:leading-[30.8px] mb-6 md:mb-12 text-white md:text-[#706458E5]`}>We provide everyone with modern, trendy, quality furniture</p>
-              <a href='#hero-section-two' className='flex justify-center md:hidden'>
-                <ArrowBrownDown color='white' />
-              </a>
-              <a href='#hero-section-two' className='hidden md:block'>
-                <ArrowBrownDown />
-              </a>
-            </div>
-          </div>
-          <div className='order-1 md:order-2 w-full h-[667px] md:h-screen overflow-hidden relative'>
-            <div className='hidden md:block absolute right-[40px] top-[40px]'>
-              <Contact />
-            </div>
-            <Image src={HeroImage} alt='Hero' priority fill />
-          </div>
-        </div>
-      </section>
+      <Hero />
       {/* hero part two */}
       <section className='w-full bg-[#F3EEE8]' id='hero-section-two'>
         <div className='max-w-[1440px] my-0 mx-auto block md:flex gap-12 px-8 md:px-[150px] py-[48px] md:py-[70px]'>
