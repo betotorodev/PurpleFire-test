576fdd2c331640d0e27c4ddad89edbc8c7fd9e14
Beto Toro <betotoro0902@gmail.com>
2023-11-29 09:48:18 -0500

    style: add responsive to carousel

--- app/page.js
+++ app/page.js
@@ -93,14 +93,16 @@ export default function Home() {
         </article>
       </section>
       {/* carrousel */}
-      <section className='pt-16 pb-[70px]'>
-        <Carousel />
-        <article className='max-w-[1440px] my-0 mx-auto block md:flex justify-between md:px-[150px] mt-[40px]'>
-          <div className='flex gap-[30px]'>
+      <section className='flex flex-col md:pt-16 pb-[30px] md:pb-[70px]'>
+        <div className='order-2 md:order-1'>
+          <Carousel />
+        </div>
+        <article className='order-1 md:order-2 md:max-w-[1440px] md:my-0 block md:flex justify-between px-8 md:px-[150px] pb-[48px] md:pb-0 md:mt-[40px] md:mb-16'>
+          <div className='block md:flex gap-[30px]'>
             <h2 className={`${merriweather.variable} font-serif text-[35px] w-[360px] text-[#121212] font-light mb-[15px] tracking-[-0.3px] leading-[40px]`}>Inspirations</h2>
             <p className={`${varta.variable} font-sans font-normal w-[359px] text-[#706458E5] text-lg leading-[25px]`}>Our experts are keen to stay on top of trends in order to offer you new inspirations for your interior and exterior every day. Remember that to inspire you we have to inspire ourselves and we want to share that with you.</p>
           </div>
-          <div>
+          <div className='hidden md:block'>
             <div className='flex gap-[11px] mb-5'>
               <ArrowBrownLeft />
               <ArrowBrownRight />
