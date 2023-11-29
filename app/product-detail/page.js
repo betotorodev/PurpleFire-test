1afb3772ce183b0718415cfb47e6e6393c4ff85c
Beto Toro <betotoro0902@gmail.com>
2023-11-29 14:03:22 -0500

    style: finish first attemp to float menu

--- app/product-detail/page.js
+++ app/product-detail/page.js
@@ -153,7 +153,7 @@ export default function ProductDetail() {
         <h2 className='text-[10px] md:text-lg font-light md:tracking-[3.6px] md:leading-[32px] text-[#191919]'>HOME / PRODUCT / TV / COLLECTION</h2>
       </section>
       {/* Product */}
-      <section className='flex flex-col md:flex-row gap-[35px] px-[19px] py-[10px] md:pl-[50px] md:pt-[82px] md:pr-[113px] md:pb-[104px]'>
+      <section className='flex flex-col md:flex-row gap-[18px] md:gap-[35px] px-[19px] py-[10px] md:pl-[50px] md:pt-[82px] md:pr-[113px] md:pb-[104px]'>
         <article className='flex flex-col items-center md:items-start md:flex-row gap-8 md:gap-3'>
           <ul className='order-2 md:order-1 flex items-center flex-row md:flex-col gap-3 md:gap-[30px] mt-[11px] md:mt-0'>
             <div className='block md:hidden p-[2px] bg-[#D9D9D9] mr-[9px]'>
@@ -185,20 +185,28 @@ export default function ProductDetail() {
             <span className='absolute -bottom-7 md:-bottom-14 left-[17%] md:left-0 text-[10px] md:text-[15px] text-[#6F6F6F] font-light'>*LG C2 42 (106cm) 4K Smart OLED evo TV</span>
           </div>
         </article>
-        <article className='flex flex-col gap-5'>
-          <div className='flex flex-col gap-[13px]'>
-            <span>Brand: LG</span>
-            <span>Model: OLED42C2PSA</span>
-            <span>Availability: Only 2 in Stock</span>
+        <article className='flex flex-col gap-2 md:gap-5'>
+          <div className='flex flex-col gap-[5px] md:gap-[13px]'>
+            <small className='text-[10px] md:text-lg font-light'><span className='font-normal'>Brand:</span> LG</small>
+            <small className='text-[10px] md:text-lg font-light'><span className='font-normal'>Model:</span> OLED42C2PSA</small>
+            <small className='text-[10px] md:text-lg font-light'><span className='font-normal'>Availability:</span> Only 2 in Stock</small>
           </div>
-          <h1 className='text-[32px] text-[#191919] font-medium'>LG C2 42 (106cm) 4K Smart OLED evo TV | WebOS | Cinema HDR</h1>
-          <div class='flex items-center gap-[10px] ml-4'>
+          <h1 className='text-sm md:text-[32px] md:leading-[40px] text-[#191919] font-medium'>LG C2 42 (106cm) 4K Smart OLED evo TV | WebOS | Cinema HDR</h1>
+          <div class='hidden md:flex items-center gap-[10px] ml-4'>
+            <StarFill />
             <StarFill />
             <StarFill />
             <StarFill />
             <StarEmpty />
           </div>
-          <ul className='list-disc ml-8'>
+          <div class='flex md:hidden items-center gap-[10px] md:ml-4'>
+            <StarFill width='10' height='10' />
+            <StarFill width='10' height='10' />
+            <StarFill width='10' height='10' />
+            <StarFill width='10' height='10' />
+            <StarEmpty width='10' height='10' />
+          </div>
+          <ul className='list-disc ml-4 md:ml-8 text-[11px] md:text-base'>
             <li>α9 Gen5 AI Processor with AI Picture Pro & AI 4K Upscaling</li>
             <li>Pixel Dimming, Perfect Black, 100% Color Fidelity & Color Volume</li>
             <li>Hands-free Voice Control, Always Ready</li>
@@ -206,32 +214,34 @@ export default function ProductDetail() {
             <li>Eye Comfort Display: Low-Blue Light, Flicker-Free</li>
           </ul>
           <div className='w-full border border-[#F0F0F0] my-[7px]' />
-          <div className='grid grid-cols-3 grid-rows-2'>
-            <span className='py-[19px] px-12 text-lg font-normal text-[#E73C17] border border-[#E73C17]'>106 cm (42)</span>
-            <span className='py-[19px] px-12 text-lg font-normal text-[#6F6F6F]'>106 cm (42)</span>
-            <span className='py-[19px] px-12 text-lg font-normal text-[#6F6F6F]'>106 cm (42)</span>
-            <span className='py-[19px] px-12 text-lg font-normal text-[#6F6F6F]'>106 cm (42)</span>
-            <span className='py-[19px] px-12 text-lg font-normal text-[#6F6F6F]'>106 cm (42)</span>
-            <span className='py-[19px] px-12 text-lg font-normal text-[#6F6F6F]'>106 cm (42)</span>
+          <div className='flex flex-wrap gap-4 md:grid md:grid-cols-3 md:grid-rows-2'>
+            <span className='py-1 md:py-[19px] px-1 md:px-12 text-[11px] md:text-lg font-normal text-[#E73C17] border border-[#E73C17]'>106 cm (42)</span>
+            <span className='py-1 md:py-[19px] px-1 md:px-12 text-[11px] md:text-lg font-normal text-[#6F6F6F]'>106 cm (42)</span>
+            <span className='py-1 md:py-[19px] px-1 md:px-12 text-[11px] md:text-lg font-normal text-[#6F6F6F]'>106 cm (42)</span>
+            <span className='py-1 md:py-[19px] px-1 md:px-12 text-[11px] md:text-lg font-normal text-[#6F6F6F]'>106 cm (42)</span>
+            <span className='py-1 md:py-[19px] px-1 md:px-12 text-[11px] md:text-lg font-normal text-[#6F6F6F]'>106 cm (42)</span>
+            <span className='py-1 md:py-[19px] px-1 md:px-12 text-[11px] md:text-lg font-normal text-[#6F6F6F]'>106 cm (42)</span>
           </div>
           <div className='w-full border border-[#F0F0F0] my-[7px]' />
-          <section className='flex w-full justify-between items-center'>
-            <div>
-              <small className='text-base mb-[13px]'>USD(incl. of all taxes)</small>
+          <div className='fixed md:relative left-0 bottom-0 w-full px-[21px] md:px-0 py-[10px] md:py-0 bg-white z-20'>
+            <section className='flex w-full justify-between items-center md:mb-[23px]'>
               <div>
-                <span className='text-[32px] font-normal mr-3'>$600.72</span>
-                <span className='text-2xl text-[#D9D9D9]'>$800.00</span>
+                <small className='text-[10px] md:text-base mb-[5px] md:mb-[13px]'>USD(incl. of all taxes)</small>
+                <div>
+                  <span className='text-lg md:text-[32px] font-normal mr-3'>$600.72</span>
+                  <span className='text-base md:text-2xl text-[#D9D9D9]'>$800.00</span>
+                </div>
               </div>
+              <div className='flex h-[21px] md:h-[60px] border'>
+                <button className='border-r h-full flex items-center justify-center px-[3px] md:px-[21px]'>-</button>
+                <span className='h-full flex items-center justify-center px-[3px] md:px-[21px]'>1</span>
+                <button className='border-l h-full flex items-center justify-center px-[3px] md:px-[21px]'>+</button>
+              </div>
+            </section>
+            <div className='flex w-full justify-between items-center'>
+              <button className='w-[120px] md:w-[202px] py-[5px] md:py-[19px] bg-[#E73C17] text-white'>Buy Now</button>
+              <button className='w-[120px] md:w-[202px] py-[5px] md:py-[19px] border text-[#E73C17] border-[#E73C17]'>Add to Cart</button>
             </div>
-            <div className='flex h-[60px] border'>
-              <button className='border-r h-full flex items-center justify-center px-[21px]'>-</button>
-              <span className='h-full flex items-center justify-center px-[21px]'>1</span>
-              <button className='border-l h-full flex items-center justify-center px-[21px]'>+</button>
-            </div>
-          </section>
-          <div className='flex w-full justify-between items-center'>
-            <button className='w-[202px] py-[19px] bg-[#E73C17] text-white'>Buy Now</button>
-            <button className='w-[202px] py-[19px] border text-[#E73C17] border-[#E73C17]'>Add to Cart</button>
           </div>
         </article>
       </section>
--- app/ui/icons/star-fill.js
+++ app/ui/icons/star-fill.js
@@ -1,5 +1,5 @@
-export const StarFill = () => (
-  <svg xmlns='http://www.w3.org/2000/svg' width='30' height='28' viewBox='0 0 30 28' fill='none'>
+export const StarFill = ({ width = '30', height = '28' }) => (
+  <svg xmlns='http://www.w3.org/2000/svg' width={width} height={height} viewBox='0 0 30 28' fill='none'>
     <path d='M15 0L18.3677 10.3647H29.2658L20.4491 16.7705L23.8168 27.1353L15 20.7295L6.18322 27.1353L9.55093 16.7705L0.734152 10.3647H11.6323L15 0Z' fill='#E73C17' />
   </svg>
 )
