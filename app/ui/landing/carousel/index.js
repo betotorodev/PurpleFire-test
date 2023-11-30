d04bb7158f45562b33b63cc831fa34d27cfcea2d
Beto Toro <betotoro0902@gmail.com>
2023-11-29 20:32:42 -0500

    fix: adjust some margins

--- app/ui/landing/carousel/index.js
+++ app/ui/landing/carousel/index.js
@@ -9,12 +9,12 @@ export const Carousel = () => {
       <div className='order-2 md:order-1'>
         <CarouselComponent />
       </div>
-      <article className='order-1 md:order-2 md:max-w-[1440px] md:my-0 block md:flex justify-between px-8 md:px-[150px] pb-[48px] md:pb-0 md:mt-[40px] md:mb-16'>
+      <article className='order-1 md:order-2 md:max-w-[1440px] md:mx-auto md:my-0 block md:flex justify-between px-8 md:px-[150px] pb-[48px] md:pb-0 md:mt-[40px] md:mb-16'>
         <div className='block md:flex gap-[30px]'>
           <h2 className={`${merriweather.variable} font-serif text-[35px] w-[360px] text-[#121212] font-light mb-[15px] tracking-[-0.3px] leading-[40px]`}>Inspirations</h2>
           <p className={`${varta.variable} font-sans font-normal w-[359px] text-[#706458E5] text-lg leading-[25px]`}>Our experts are keen to stay on top of trends in order to offer you new inspirations for your interior and exterior every day. Remember that to inspire you we have to inspire ourselves and we want to share that with you.</p>
         </div>
-        <div className='hidden md:block'>
+        <div className='hidden md:block md:ml-[30px]'>
           <div className='flex gap-[11px] mb-5'>
             <ArrowBrownLeft />
             <ArrowBrownRight />
